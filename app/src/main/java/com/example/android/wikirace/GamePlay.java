c869a7843ff68fabc218b073b387b2472a416fff
David Jung <d.jung460@gmail.com>
2016-02-20 14:54:02 -0800

    Add back button for webview

--- app/src/main/java/com/example/android/wikirace/GamePlay.java
+++ app/src/main/java/com/example/android/wikirace/GamePlay.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,20 +18,30 @@ import java.util.List;
 public class GamePlay extends AppCompatActivity {
     private WebView mWikiWebView;
     private TextView mDestArticleTitle;
+    private ImageButton mBackButton;
     private List<Article> articles = new ArrayList<>();
-    private Bundle savedInstace;
 
     private ArticleAsyncTask wikiQueryTask = (ArticleAsyncTask) new ArticleAsyncTask(new ArticleAsyncTask.AsyncResponse() {
         @Override
         public void processFinish(List<Article> output) {
             articles = output;
             mWikiWebView = (WebView) findViewById(R.id.wiki_web_view);
+            mBackButton = (ImageButton) findViewById(R.id.back_button);
             mDestArticleTitle = (TextView) findViewById(R.id.dest_article_title_textview);
             mDestArticleTitle.setText(articles.get(1).getTitle());
 
             mWikiWebView.loadUrl(articles.get(0).getUrl());
             GameWebViewClient client = new GameWebViewClient(articles.get(1));
             mWikiWebView.setWebViewClient(client);
+
+            mBackButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (mWikiWebView.canGoBack()) {
+                        mWikiWebView.goBack();
+                    }
+                }
+            });
         }
     }).execute("EXECUTE");
 
@@ -39,6 +51,5 @@ public class GamePlay extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
-        savedInstace = savedInstanceState;
     }
 }
